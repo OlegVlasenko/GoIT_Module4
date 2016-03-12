c76d4d944f66bc3c3c9699ab4009d4ac13a11f8c
OlegVlasenko <vlasenko.inbox@gmail.com>
2016-03-12 21:42:09 +0200

    const

--- com/company/Temperature.java
+++ com/company/Temperature.java
@@ -5,12 +5,15 @@ package com.company;
  */
 abstract public class Temperature {
 
+    static final double CONST_9div5 = 9.0d / 5.0d;
+    static final double CONST_5div9 = 5.0d / 9.0d;
+
     static double CtoF(double tempC) {
-        return ( 9.0d / 5.0d ) * tempC + 32.0d;
+        return CONST_9div5 * tempC + 32.0d;
     }
 
     static double FtoC(double tempF) {
-        return ( 5.0d / 9.0d ) * (tempF - 32.0d);
+        return CONST_5div9 * (tempF - 32.0d);
     }
 
 }
